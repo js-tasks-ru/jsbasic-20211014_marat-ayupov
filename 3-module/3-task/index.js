9e0f0a34920126ffef65ebb842096cfa5663acfe
marat <marat84ay@gmail.com>
2021-10-28 18:56:34 +0500

    Task in progress Переведите текст вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,23 @@
 function camelize(str) {
-  // ваш код...
+  let strings = str.split('-');
+  let arrayStrings = [];
+  console.log(strings);
+
+  strings.forEach((element, index) => {
+    arrayStrings.push(element.slice(0, 1).toUpperCase() + element.slice(1));
+  });
+
+  let joinString = arrayStrings.join('');
+
+  if (str.slice(0, 1) !== '-') {
+    joinString = joinString.slice(0, 1).toLowerCase() + joinString.slice(1);
+  }
+
+  return joinString;
 }
+
+function sliceUpper(string) {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+}
+
+console.log(camelize('background-color-red'));
