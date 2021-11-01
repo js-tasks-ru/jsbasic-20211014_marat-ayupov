66674a6b8a5cbead2b709e2e7b88534580871ebd
marat84 <46667902+marat84@users.noreply.github.com>
2021-11-01 23:19:43 +0500

    Task in progress Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,17 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+  const fragment = document.createDocumentFragment();
+
+  console.log(friends);
+
+  for (let friend of friends) {
+    let li = document.createElement('li');
+    li.append([friend.firstName, friend.lastName].join(' '));
+
+    fragment.appendChild(li);
+  }
+
+  console.log(fragment);
+
+  return ul.appendChild(fragment);
 }
