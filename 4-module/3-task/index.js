81d3e5bf2f4965ac7d5e789e0f4dbdf8269d9984
marat <marat84ay@gmail.com>
2021-11-02 11:15:55 +0500

    Task complete Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,46 @@
 function highlight(table) {
-  // ваш код...
+  const tdElements = table.querySelectorAll('td');
+  let statusCell;
+  let genderCell;
+  let ageCell;
+
+  for (let tdElement of tdElements) {
+    if (tdElement.closest('thead')) {
+      if (tdElement.textContent === 'Status') {
+        statusCell = tdElement.cellIndex;
+      }
+
+      if (tdElement.textContent === 'Gender') {
+        genderCell = tdElement.cellIndex;
+      }
+
+      if (tdElement.textContent === 'Age') {
+        ageCell = tdElement.cellIndex;
+      }
+    }
+  }
+
+  for (let tdElement of tdElements) {
+    if (tdElement.closest('tbody')) {
+      if (tdElement.cellIndex === statusCell) {
+        if (!tdElement.dataset.available) {
+          tdElement.parentElement.setAttribute('hidden', 'true');
+        } else {
+          tdElement.parentElement.classList.add(
+            (tdElement.dataset.available === 'true') ? 'available' : 'unavailable'
+          );
+        }
+      }
+
+      if (tdElement.cellIndex === genderCell) {
+        tdElement.parentElement.classList.add(
+          (tdElement.textContent === 'm') ? 'male' : 'female'
+        );
+      }
+
+      if (tdElement.cellIndex === ageCell && +tdElement.textContent < 18) {
+        tdElement.parentElement.style.textDecoration = 'line-through';
+      }
+    }
+  }
 }
