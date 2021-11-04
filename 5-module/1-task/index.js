48f254a4793887a5d807ee2a7e0cacd82f52b34a
marat <marat84ay@gmail.com>
2021-11-04 11:17:14 +0500

    Task complete Спрятать себя

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,15 @@
 function hideSelf() {
-  // ваш код...
+  // let buttons = document.querySelectorAll('.hide-self-button');
+
+  /*for (let button of buttons) {
+    button.addEventListener('click', (e) => {
+      e.currentTarget.hidden = true;
+    });
+  }*/
+
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('hide-self-button')) {
+      e.target.hidden = true;
+    }
+  });
 }
